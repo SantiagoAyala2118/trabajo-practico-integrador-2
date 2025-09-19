9fde3de95cb406380fb5a1276dacdac2c601debe
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-19 16:40:19 -0300

    Corrección de errores a la hora de conectar con la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 export const startDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb://127.0.0.1:27017/${process.env.MONGODB_URI}`
-    );
-    // await mongoose.connection.dropDatabase();
+    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connection.dropDatabase();
     console.log("Database connected succesfuly");
   } catch (err) {
     console.error("Error conecting to the database", err);
