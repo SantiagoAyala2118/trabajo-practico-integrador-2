fc74efce67942a5673649e72302fda2801716ad3
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-19 15:03:55 -0300

    Configuraciones del packaje json hechas y servidor básico levantado

---
+++ app.js
@@ -0,0 +1,28 @@
+//IMPORTACIONES
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT;
+
+//MIDDLEWARES
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//LEVANTAMIENTO DEL SERVIDOR
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
