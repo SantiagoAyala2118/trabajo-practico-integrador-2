4d473c57cb3f70ddeba4e020fbbac0ee52e73e69
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-19 16:41:24 -0300

    Se avanzó en la función del login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/user.model.js";
+import { generateToken } from "../helpers/jwt.helper.js";
 
 export const register = async (req, res) => {
   const { username, email, password, role, profile } = req.body;
@@ -20,11 +21,42 @@ export const register = async (req, res) => {
   } catch (err) {
     console.error("Server error", err);
     return res.status(500).json({
+      ok: false,
       message: "Server error",
     });
   }
 };
 
-export const loging = async (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        message: "User not found",
+      });
+    }
+
+    const generateToken = generateToken(user);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    });
+
+    return res.status(200).json({
+      ok: true,
+      message: "Loggin succesfuly",
+    });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+    });
+  }
+};
 
 export const logout = async (req, res) => {};
