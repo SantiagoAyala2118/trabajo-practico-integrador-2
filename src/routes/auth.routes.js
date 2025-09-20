9bd9a39fae896ecf1f8ea6f810938bcc384f531a
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-20 20:28:54 -0300

    Implementación de los middlewares en las rutas correspondientes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,11 +11,21 @@ import {
 
 //MIDDLEWARES
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import {
+  registerValidations,
+  updateAuthProfileValidations,
+} from "../middlewares/validations/auth.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const authRoutes = Router();
 
 //ENDPOINTS
-authRoutes.post("/auth/register", register);
+authRoutes.post(
+  "/auth/register",
+  registerValidations,
+  applyValidations,
+  register
+);
 
 authRoutes.post("/auth/login", login);
 
@@ -23,6 +33,12 @@ authRoutes.post("/auth/logout", authMiddleware, logout);
 
 authRoutes.get("/auth/profile", authMiddleware, getAuthProfile);
 
-authRoutes.put("/auth/profile", authMiddleware, updateAuthProfile);
+authRoutes.put(
+  "/auth/profile",
+  authMiddleware,
+  updateAuthProfileValidations,
+  applyValidations,
+  updateAuthProfile
+);
 
 export default authRoutes;
