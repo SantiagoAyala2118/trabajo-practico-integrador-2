a6a85742a9ba6c5f7c51f98752bb60bfac10151b
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-20 15:21:17 -0300

    Totalidad de routers importados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import tagRoutes from "./tag.routes.js";
+import articleRoutes from "./article.routes.js";
+import commentRoutes from "./comment.routes.js";
+import articleTagRoutes from "./article_tag.routes.js";
 
 const routes = Router();
 
@@ -9,5 +13,17 @@ routes.use(authRoutes);
 
 //RUTAS DE USER
 routes.use(userRoutes);
+
+//RUTAS DE TAG
+routes.use(tagRoutes);
+
+//RUTAS DE ARTICLE
+routes.use(articleRoutes);
+
+//RUTAS DE COMMENT
+routes.use(commentRoutes);
+
+//RUTAS DE ARTICLE_TAG
+routes.use(articleTagRoutes);
 
 export default routes;
