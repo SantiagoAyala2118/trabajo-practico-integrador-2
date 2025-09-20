7e2b7faf9e6ffb9ddbdc4526096b2b1eb5492321
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-19 21:50:54 -0300

    Rutas de perfil añadidas

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  register,
+  logout,
+  updateAuthProfile,
+  getAuthProfile,
+} from "../controllers/auth.controller.js";
+
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const authRoutes = Router();
 
@@ -7,6 +15,10 @@ authRoutes.post("/auth/register", register);
 
 authRoutes.post("/auth/login", login);
 
-authRoutes.post("/auth/logout", logout);
+authRoutes.post("/auth/logout", authMiddleware, logout);
+
+authRoutes.get("/auth/profile", authMiddleware, getAuthProfile);
+
+authRoutes.put("/auth/profile", authMiddleware, updateAuthProfile);
 
 export default authRoutes;
