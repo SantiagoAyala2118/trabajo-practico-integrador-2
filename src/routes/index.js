1b346707ee816d4a890d5db9d01849fd20d82313
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-19 23:37:32 -0300

    Ruta de users añadida

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import userRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 
 const routes = Router();
 
 //RUTAS DE AUTH
 routes.use(authRoutes);
+
+//RUTAS DE USER
+routes.use(userRoutes);
 
 export default routes;
