58515618e5de385f3bb8832251011275a187a8ee
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-20 07:40:08 -0300

    Controladores solicitados funcionando

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,9 +4,9 @@ import { CommentModel } from "../models/comment.model.js";
 
 export const getAllUsersWithArticles = async (req, res) => {
   try {
-    const users = await UserModel.find().populate({
+    const users = await UserModel.find({ deletedAt: null }).populate({
       path: "articles",
-      select: "title status",
+      select: "title status _id",
     });
 
     if (users) {
@@ -28,20 +28,71 @@ export const getAllUsersWithArticles = async (req, res) => {
 export const getUserWithArticlesAndComments = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await UserModel.findById(id).populate({
-      path: "articles",
-      select: "title content status ",
-      populate: {
-        path: "comments",
-        select: "content author",
-      },
-    });
+    const user = await UserModel.findOne({ _id: id, deletedAt: null }).populate(
+      {
+        path: "articles",
+        select: "title status _id",
+        populate: {
+          path: "comments",
+          select: "content author",
+        },
+      }
+    );
 
     return res.status(200).json({
       ok: true,
       message: "User found",
       User: user,
     });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      ok: true,
+      message: "User updated",
+      user: updateUser,
+    });
+  } catch (err) {
+    console.error("Server error", err);
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await UserModel.findByIdAndUpdate(
+      id,
+      {
+        $set: { deletedAt: new Date() },
+      },
+      { new: true }
+    );
+
+    if (deletedUser) {
+      return res.status(200).json({
+        ok: true,
+        message: "User deleted",
+      });
+    }
   } catch (err) {
     console.error("Server error", err);
     return res.status(500).json({
