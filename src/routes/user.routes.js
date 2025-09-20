dce38aac0de0322879be162410245d54be7bb875
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-20 07:40:28 -0300

    Rutas restantes añadidas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,16 @@
 import Router from "express";
 import {
+  deleteUser,
   getAllUsersWithArticles,
   getUserWithArticlesAndComments,
+  updateUser,
 } from "../controllers/user.controller.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/users", getAllUsersWithArticles);
 userRoutes.get("/users/:id", getUserWithArticlesAndComments);
-// userRoutes.put("/users/:id");
-// userRoutes.delete("/users/:id");
+userRoutes.put("/users/:id", updateUser);
+userRoutes.delete("/users/:id", deleteUser);
 
 export default userRoutes;
