1ca4505e66a02e9c85f6c7b9d0dc56aa9030d3f5
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-21 13:52:51 -0300

    Validaciones terminadas

---
+++ src/middlewares/validations/article_tag.validations.js
@@ -0,0 +1,73 @@
+import { param } from "express-validator";
+import { ArticleModel } from "../../models/article.model.js";
+import { TagModel } from "../../models/tag.model.js";
+
+export const addTagToArticleValidations = [
+  param("articleId")
+    .trim()
+    .isLength({ min: 1, max: 24 })
+    .withMessage("ArticleId must be an aplhanumeric with 24 characters")
+    .custom(async (articleId) => {
+      try {
+        const articleExisting = await ArticleModel.findById(articleId);
+
+        if (!articleExisting) {
+          return Promise.reject("Article not founded");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the article", err);
+        return Promise.reject("Error checking the existency of the article");
+      }
+    }),
+  param("tagId")
+    .trim()
+    .isLength({ min: 1, max: 24 })
+    .withMessage("The id must be an alphanumeric with 24 characters")
+    .custom(async (tagId) => {
+      try {
+        const tag = await TagModel.finById(tagId);
+
+        if (!tag) {
+          return Promise.reject("Tag not founded");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the tag", err);
+        return Promise.reject("Error checking the existency of the tag");
+      }
+    }),
+];
+
+export const removeTagFromArticleValidations = [
+  param("articleId")
+    .trim()
+    .isLength({ min: 1, max: 24 })
+    .withMessage("ArticleId must be an aplhanumeric with 24 characters")
+    .custom(async (articleId) => {
+      try {
+        const articleExisting = await ArticleModel.findById(articleId);
+
+        if (!articleExisting) {
+          return Promise.reject("Article not founded");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the article", err);
+        return Promise.reject("Error checking the existency of the article");
+      }
+    }),
+  param("tagId")
+    .trim()
+    .isLength({ min: 1, max: 24 })
+    .withMessage("The id must be an alphanumeric with 24 characters")
+    .custom(async (tagId) => {
+      try {
+        const tag = await TagModel.finById(tagId);
+
+        if (!tag) {
+          return Promise.reject("Tag not founded");
+        }
+      } catch (err) {
+        console.error("Error checking the existency of the tag", err);
+        return Promise.reject("Error checking the existency of the tag");
+      }
+    }),
+];
