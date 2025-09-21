d3342a7d0e29829f6ed23721d14fc1c40ca4b2e7
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-21 10:41:33 -0300

    Middlewares de validaciones añadidos

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,16 +11,48 @@ import {
 //MIDDLEWARES
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { adminMiddleware } from "../middlewares/adminMiddleware.js";
+import {
+  getUserWithArticlesAndCommentsValidations,
+  updateUserValidations,
+  deleteUserValidations,
+} from "../middlewares/validations/user.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const userRoutes = Router();
 
 //ENDPOINTS
-userRoutes.get("/users", adminMiddleware, getAllUsersWithArticles);
+userRoutes.get(
+  "/users",
+  authMiddleware,
+  adminMiddleware,
+  getAllUsersWithArticles
+);
 
-userRoutes.get("/users/:id", adminMiddleware, getUserWithArticlesAndComments);
+userRoutes.get(
+  "/users/:id",
+  authMiddleware,
+  adminMiddleware,
+  getUserWithArticlesAndCommentsValidations,
+  applyValidations,
+  getUserWithArticlesAndComments
+);
 
-userRoutes.put("/users/:id", adminMiddleware, updateUser);
+userRoutes.put(
+  "/users/:id",
+  authMiddleware,
+  adminMiddleware,
+  updateUserValidations,
+  applyValidations,
+  updateUser
+);
 
-userRoutes.delete("/users/:id", adminMiddleware, deleteUser);
+userRoutes.delete(
+  "/users/:id",
+  authMiddleware,
+  adminMiddleware,
+  deleteUserValidations,
+  applyValidations,
+  deleteUser
+);
 
 export default userRoutes;
