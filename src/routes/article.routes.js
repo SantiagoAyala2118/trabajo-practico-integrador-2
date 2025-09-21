72a12f5eddc2a5cbfa302f163d9f74dc389e62d1
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-21 12:07:24 -0300

    Middleware de validaciones añadidos

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -14,21 +14,44 @@ import {
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { articleOwnerAdminMiddleware } from "../middlewares/articleOwnerOrAdmin.js";
 
+import {
+  createArticleValidations,
+  getArticleByIdValidations,
+  updateArticleValidations,
+  deleteArticleValidations,
+} from "../middlewares/validations/article.validations.js";
+
+import { applyValidations } from "../middlewares/validator.js";
+
 const articleRoutes = Router();
 
 //ENDPOINTS
-articleRoutes.post("/articles", authMiddleware, createArticle);
+articleRoutes.post(
+  "/articles",
+  authMiddleware,
+  createArticleValidations,
+  applyValidations,
+  createArticle
+);
 
 articleRoutes.get("/articles", authMiddleware, getAllArticles);
 
 articleRoutes.get("/articles/my", authMiddleware, getUserLoggedArticles);
 
-articleRoutes.get("/articles/:id", authMiddleware, getArticle);
+articleRoutes.get(
+  "/articles/:id",
+  authMiddleware,
+  getArticleByIdValidations,
+  applyValidations,
+  getArticle
+);
 
 articleRoutes.put(
   "/articles/:id",
   authMiddleware,
   articleOwnerAdminMiddleware,
+  updateArticleValidations,
+  applyValidations,
   updateArticle
 );
 
@@ -36,6 +59,8 @@ articleRoutes.delete(
   "/articles/:id",
   authMiddleware,
   articleOwnerAdminMiddleware,
+  deleteArticleValidations,
+  applyValidations,
   deleteArticle
 );
 
